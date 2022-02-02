b48f77f7351963cb6f8345e53fda87326877be15
DigZator <omsbhise@gmail.com>
2022-02-02 10:29:28 +0530

    Added histories in trainer()

--- PolGrad.py
+++ PolGrad.py
@@ -50,6 +50,36 @@ def trainer(AN, CN, nE):
 	optimA = torch.optim.Adam(actor.parameters(), lr = 0.01)
 	optimC = torch.optim.Adam(critic.parameters(), lr = 0.01)
 	E = 0
+	lengths = []
 	while(E>nE):
 		S = env.reset()
+		
+		h_S = []
+		h_ln_p = []
+		h_act = []
+		h_crit = []
+		h_rev = []
 
+
+		length = 0
+		done = False
+		while not done:
+			S = torch.FloatTensor(S)
+			dist = Actor_Network(S)
+			crit = Critic_Network(S)
+
+			dist_num = dist.detach().numpy().squeeze()
+			action = np.random.choice(n_op, p = dist_num())
+			ln_p = torch.log(dist.squeeze(0)[action])
+			#Call for the critic, to calculate gradient, return next state, done, 
+
+			nS, R, done _ = env.step()
+
+			h_S.append(S)
+			h_act.append(action)
+			h_rev.append(R)
+
+
+actor = Actor_Network(2,2)
+
+print(actor(torch.FloatTensor([5,6])), actor(torch.FloatTensor([5,6])).detach().numpy().squeeze())
