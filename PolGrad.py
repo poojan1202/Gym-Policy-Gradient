888f2d1a9bd59e745000e4ad85bedff1ac6abcf8
Poojan Gandhi <poojangandhi001@gmail.com>
2022-02-03 20:43:43 +0530

    added episode generation code

--- PolGrad.py
+++ PolGrad.py
@@ -5,6 +5,7 @@ import torch.nn.functional as functions
 import random
 import matplotlib.pyplot as plt
 import numpy as np
+from torch.distributions.normal import Normal
 
 class Actor_Network(nn.Module):
     """
@@ -23,6 +24,7 @@ class Actor_Network(nn.Module):
         x = functions.relu(self.linear2(x))
         x = functions.relu(self.linear3(x))
         x = functions.relu(self.linear4(x))
+       #x = self.linear4(x)
         return x
 
 class Critic_Network(nn.Module):
@@ -46,40 +48,29 @@ class Critic_Network(nn.Module):
 
 #class Agent():
 
-def trainer(AN, CN, nE):
-	optimA = torch.optim.Adam(actor.parameters(), lr = 0.01)
-	optimC = torch.optim.Adam(critic.parameters(), lr = 0.01)
-	E = 0
-	lengths = []
-	while(E>nE):
-		S = env.reset()
-		
-		h_S = []
-		h_ln_p = []
-		h_act = []
-		h_crit = []
-		h_rev = []
-
-
-		length = 0
-		done = False
-		while not done:
-			S = torch.FloatTensor(S)
-			dist = Actor_Network(S)
-			crit = Critic_Network(S)
-
-			dist_num = dist.detach().numpy().squeeze()
-			action = np.random.choice(n_op, p = dist_num())
-			ln_p = torch.log(dist.squeeze(0)[action])
-			#Call for the critic, to calculate gradient, return next state, done, 
-
-			nS, R, done _ = env.step()
-
-			h_S.append(S)
-			h_act.append(action)
-			h_rev.append(R)
 
 
 actor = Actor_Network(2,2)
 
 print(actor(torch.FloatTensor([5,6])), actor(torch.FloatTensor([5,6])).detach().numpy().squeeze())
+
+
+env = gym.make('Pendulum-v0')
+actor_model = Actor_Network(3,2)
+critic_model = Critic_Network(3)
+
+state = env.reset()
+print('state',state)
+done=False
+while not done:
+    with torch.no_grad():
+        dist_param = actor_model(torch.from_numpy(state).float())
+        dist = Normal(dist_param[0],dist_param[1])
+        #action = dist.sample().item()
+        action = dist.sample().numpy().reshape(1)
+        #print(action)
+    new_state, reward, done, info = env.step(action)
+    state = new_state
+
+
+
