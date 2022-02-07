0dabb22a48f2f40663b99f0036f4436074b1f9c0
Poojan Gandhi <poojangandhi001@gmail.com>
2022-02-07 13:35:58 +0530

    Added Training Code

--- PolGrad.py
+++ PolGrad.py
@@ -1,11 +1,12 @@
 import gym
 import torch
 import torch.nn as nn
-import torch.nn.functional as functions
+import torch.nn.functional as F
 import random
 import matplotlib.pyplot as plt
 import numpy as np
 from torch.distributions.normal import Normal
+from torch.autograd import Variable
 
 class Actor_Network(nn.Module):
     """
@@ -20,10 +21,10 @@ class Actor_Network(nn.Module):
         self.linear4 = nn.Linear(8,output_layer)
 
     def forward(self,x):
-        x = functions.relu(self.linear1(x))
-        x = functions.relu(self.linear2(x))
-        x = functions.relu(self.linear3(x))
-        x = functions.relu(self.linear4(x))
+        x = F.relu(self.linear1(x))
+        x = F.relu(self.linear2(x))
+        x = F.relu(self.linear3(x))
+        x = F.relu(self.linear4(x))
        #x = self.linear4(x)
         return x
 
@@ -40,9 +41,9 @@ class Critic_Network(nn.Module):
         
 
     def forward(self,x):
-        x = functions.relu(self.linear1(x))
-        x = functions.relu(self.linear2(x))
-        x = functions.relu(self.linear3(x))
+        x = F.relu(self.linear1(x))
+        x = F.relu(self.linear2(x))
+        x = F.relu(self.linear3(x))
         x = self.linear4(x)
         return x
 
@@ -52,25 +53,75 @@ class Critic_Network(nn.Module):
 
 actor = Actor_Network(2,2)
 
-print(actor(torch.FloatTensor([5,6])), actor(torch.FloatTensor([5,6])).detach().numpy().squeeze())
+#print(actor(torch.FloatTensor([5,6])), actor(torch.FloatTensor([5,6])).detach().numpy().squeeze())
 
 
 env = gym.make('Pendulum-v0')
 actor_model = Actor_Network(3,2)
 critic_model = Critic_Network(3)
 
-state = env.reset()
-print('state',state)
-done=False
-while not done:
-    with torch.no_grad():
-        dist_param = actor_model(torch.from_numpy(state).float())
-        dist = Normal(dist_param[0],dist_param[1])
-        #action = dist.sample().item()
-        action = dist.sample().numpy().reshape(1)
-        #print(action)
-    new_state, reward, done, info = env.step(action)
-    state = new_state
+gamma = 0.9
+learning_rate = 0.005
+critic_optimizer = torch.optim.Adam(critic_model.parameters(),lr=learning_rate)
+actor_optimizer = torch.optim.Adam(actor_model.parameters(),lr=learning_rate)
+
+for ep in range(500):
+    state = env.reset()
+    print('state', state)
+    done = False
+    total_rew = 0
+    while not done:
+        #if ep==0:
+        #    env.render()
+        #if (ep+1)%50==0:
+        #    env.render()
+        env.render()
+        with torch.no_grad():
+            dist_param = actor_model(torch.from_numpy(state).float())
+            if dist_param[1] == 0:
+                dist_param[1] = 0.0001
+            dist = Normal(dist_param[0], dist_param[1])
+            # action = dist.sample().item()
+            action = dist.sample().numpy().reshape(1)
+            # print(state)
+            # print(dist_param)
+        new_state, rew, done, info = env.step(action)
+
+        total_rew += rew
+
+        state_val = critic_model(torch.from_numpy(state).float())
+        target_state_val = rew + gamma*critic_model(torch.from_numpy(new_state).float())
+
+        state_val = torch.FloatTensor(state_val)
+        with torch.no_grad():
+            target_state_val = torch.FloatTensor(target_state_val)
+
+        critic_loss = F.mse_loss(state_val,target_state_val)
+        critic_optimizer.zero_grad()
+        critic_loss.backward()
+        critic_optimizer.step()
+
+        with torch.no_grad():
+            Vs = critic_model(torch.FloatTensor(state))
+            Vns = critic_model(torch.FloatTensor(new_state))
+        adv_val = torch.from_numpy(np.array(rew)).float() + gamma*Vns - Vs
+
+        actor_loss = -dist.log_prob(torch.from_numpy(action).float())*adv_val
+        actor_loss = Variable(actor_loss,requires_grad=True)
+
+        actor_optimizer.zero_grad()
+        actor_loss.backward()
+        actor_optimizer.step()
+
+
+
+
+        state = new_state
+    print('total_rew',total_rew)
+
+
+
+
 
 
 
